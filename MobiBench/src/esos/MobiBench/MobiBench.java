f0fe7908a05f94bc3703be851cf8db6eaba438a8
jwGom <tearoses@hanyang.ac.kr>
2013-02-12 14:05:50 +0900

    font and text changed

--- MobiBench/src/esos/MobiBench/MobiBench.java
+++ MobiBench/src/esos/MobiBench/MobiBench.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 
 public class MobiBench extends Activity implements Runnable{
     /** Called when the activity is first created. */
-    @Override
+    
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
